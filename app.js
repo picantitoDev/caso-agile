adbefdf2d85809f19794ae2a7d003d9ca91ec1e6
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-05-07 18:26:28 -0500

    Basic app file and basic folders

--- app.js
+++ app.js
@@ -1,31 +1,88 @@
-require("dotenv").config()
+const express = require("express")
+const app = express()
+const path = require("path")
+const session = require("express-session")
+const methodOverride = require("method-override")
+const passport = require("passport")
+const flash = require("connect-flash")
 
-const url = "https://apiperu.dev/api/ruc"
-const token = process.env.API_KEY_SUNAT
-const ruc = "20141878477"
+// Configurar passport
+// require("./auth/passportConfig")
+// const validarSesion = require("./auth/authMiddleware")
+// const verificarAdmin = require("./auth/authMiddlewareAdmin")
 
-const fetchDatosRUC = async () => {
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({ ruc }),
-    })
+// Importar rutas
+// const rutaProductos = require("./routes/rutaProductos")
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
-    }
+// Middleware
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")))
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
 
-    const data = await response.json()
-    console.log("Datos del RUC desde padrón reducido de SUNAT:")
-    console.log(data)
-  } catch (error) {
-    console.error("Fetch error:", error)
-  }
-}
+// Configurar sesión
+// app.use(
+//   session({
+//     secret: "clave_super_secreta",
+//     resave: false,
+//     saveUninitialized: false,
+//   })
+// )
 
-fetchDatosRUC()
+// app.use(flash())
+
+// Hacer disponibles los mensajes flash en todas las vistas
+// app.use((req, res, next) => {
+//   res.locals.success_msg = req.flash("success_msg")
+//   res.locals.error_msg = req.flash("error_msg")
+//   res.locals.error = req.flash("error") // Passport usa 'error' por defecto
+//   next()
+// })
+
+// Inicializar passport y sesiones
+// app.use(passport.initialize())
+// app.use(passport.session())
+
+// Rutas principales
+app.get("/", (req, res) => {
+  res.send("nice")
+  // res.render("index", { user: req.user })
+})
+
+// app.use("/productos", validarSesion, rutaProductos)
+// app.use("/categorias", validarSesion, rutaCategorias)
+// app.use("/proveedores", validarSesion, rutaProveedores)
+// app.use("/usuarios", validarSesion, verificarAdmin, rutaUsuarios)
+// app.use("/movimientos", validarSesion, rutaMovimientos)
+
+// Login
+// app.post(
+//   "/log-in",
+//   passport.authenticate("local", {
+//     successRedirect: "/",
+//     failureRedirect: "/",
+//     failureFlash: true,
+//   })
+// )
+
+// // Logout
+// app.get("/log-out", (req, res, next) => {
+//   req.logout((err) => {
+//     if (err) {
+//       return next(err)
+//     }
+//     res.redirect("/")
+//   })
+// })
+
+// Middleware para manejar errores 404
+app.use((req, res, next) => {
+  res.status(404).render("404", { url: req.originalUrl })
+})
+
+// Servidor
+const PORT = 8080
+app.listen(PORT, () => {
+  console.log(`Running on port: ${PORT}...`)
+})
