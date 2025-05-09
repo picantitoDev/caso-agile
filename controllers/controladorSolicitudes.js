0b81b27a2936d48f8343439e78d2d10986c837a7
Piero Arturo <155034777+picantitoDev@users.noreply.github.com>
2025-05-09 18:48:17 -0500

    Update controladorSolicitudes.js

--- controllers/controladorSolicitudes.js
+++ controllers/controladorSolicitudes.js
@@ -87,13 +87,13 @@ async function verSolicitudesUsuario(req, res) {
         new Date(solicitud.fecha_creacion)
       ).setZone("America/Lima")
       const hoy = DateTime.now().setZone("America/Lima")
-      // const diferenciaMinutos = hoy.diff(fechaCreacion, "minutes").minutes
-      // const vencida = diferenciaMinutos >= 3
+      const diferenciaMinutos = hoy.diff(fechaCreacion, "minutes").minutes
+      const vencida = diferenciaMinutos >= 15
 
       // Calcular la diferencia en días
       // Tengo un poquito de meyo
-      const diferenciaDias = hoy.diff(fechaCreacion, "days").days
-      const vencida = diferenciaDias >= 30
+      //const diferenciaDias = hoy.diff(fechaCreacion, "days").days
+      //const vencida = diferenciaDias >= 30
 
       console.log("Hora actual servidor (UTC):", new Date().toISOString())
       console.log(
