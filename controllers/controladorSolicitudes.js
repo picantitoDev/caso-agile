3151304c5e5d00ae71440c74b3d280b2d42b8493
Piero Arturo <155034777+picantitoDev@users.noreply.github.com>
2025-05-09 13:42:49 -0500

    Update controladorSolicitudes.js

--- controllers/controladorSolicitudes.js
+++ controllers/controladorSolicitudes.js
@@ -76,8 +76,6 @@ async function verSolicitudesUsuario(req, res) {
       // Calcular la diferencia en días
       //const diferenciaDias = hoy.diff(fechaCreacion, "days").days
       //const vencida = diferenciaDias >= 30
-      console.log(`Diferencia de días: ${diferenciaDias}`)
-      console.log(`¿La fecha está vencida? ${vencida ? "Sí" : "No"}`)
 
       console.log("Hora actual servidor (UTC):", new Date().toISOString())
       console.log(
